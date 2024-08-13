92691236600753982832d8e7ee58d55df17dc6ee
Ghous Ali <ghousali002@gmail.com>
2024-08-13 22:10:25 +0500

    LoginSignup-swiper for Laptops screens

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import LoginInSignUp from './components/LoginInSignUp'
+
+function App() {
+  return (
+    <div className="App">
+      <LoginInSignUp />
+    </div>
+  );
+}
+
+export default App;
